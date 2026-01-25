3224a4dca8ca105a2b9067d21cd82c89f531b727
SATYAM KUMAR KASYAP <SATYAMKU88@GMAIL.COM>
2026-01-26 03:08:28 +0530

    Time: 1 ms (99.60%) | Memory: 77.1 MB (75.97%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        int maxsum=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum>maxsum){
+                maxsum=sum;
+            }
+           // maxsum=Math.max(sum,maxsum);
+            if(sum<0){
+                sum=0;
+            }
+        
+        }
+        return maxsum;
+    }
+}
