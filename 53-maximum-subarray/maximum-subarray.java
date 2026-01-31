23f39de93ced5760eaef9aadf2924f78b57caf3e
SATYAM KUMAR KASYAP <SATYAMKU88@GMAIL.COM>
2026-02-01 03:48:05 +0530

    Time: 1 ms (99.59%) | Memory: 77.1 MB (89.39%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -5,7 +5,6 @@ class Solution {
         int maxsum=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
             sum+=nums[i];
-           
             maxsum=Math.max(sum,maxsum);
             if(sum<0){
                 sum=0;
